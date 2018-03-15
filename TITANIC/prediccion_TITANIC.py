687e480a3b2febf67d925d5d98d5b4ba168058b4
MAURI-PROGRAM <merchan.ber.mau@gmail.com>
2018-03-15 16:48:39 -0500

    Convertir caracteristicas de testo a numeros

--- TITANIC/prediccion_TITANIC.py
+++ TITANIC/prediccion_TITANIC.py
@@ -84,8 +84,23 @@ for dataset in combine:
     dataset['Title'] = dataset['Title'].replace(['Lady', 'Countess','Capt', 'Col',\
  	'Don', 'Dr', 'Major', 'Rev', 'Sir', 'Jonkheer', 'Dona'], 'Rare')
 
-    dataset['Title'] = dataset['Title'].replace('Mlle', 'Miss')
-    dataset['Title'] = dataset['Title'].replace('Ms', 'Miss')
+    dataset['Title'] = dataset['Title'].replace(['Mlle','Ms'], 'Miss')
     dataset['Title'] = dataset['Title'].replace('Mme', 'Mrs')
     
 print(train_df[['Title', 'Survived']].groupby(['Title'], as_index=False).mean())
+
+title_mapping = {"Mr": 1, "Miss": 2, "Mrs": 3, "Master": 4, "Rare": 5}
+for dataset in combine:
+    dataset['Title'] = dataset['Title'].map(title_mapping)
+    dataset['Title'] = dataset['Title'].fillna(0)
+
+print(train_df.head())
+train_df = train_df.drop(['Name', 'PassengerId'], axis=1)
+test_df = test_df.drop(['Name'], axis=1)
+combine = [train_df, test_df]
+print(train_df.shape, test_df.shape)
+
+for dataset in combine:
+    dataset['Sex'] = dataset['Sex'].map( {'female': 1, 'male': 0} ).astype(int)
+
+print(train_df.head())
